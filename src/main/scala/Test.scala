72a56ece41cd37981b3fc61a0fc9b85c4756bde1
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2014-08-13 17:31:21 +0400

    working client-server pair with logging

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,41 +1,41 @@
 package AkkaTest1
 
 import akka.actor._
-import concurrent.duration._
 
-sealed trait Message
-case object Ping extends Message
-case class Pong(count: Int) extends Message
-case object Stop extends Message
+case object Ping
+case class Pong(count: Int)
+case object Stop
 
-class Server extends Actor {
+class Server extends Actor with ActorLogging {
   var count = 0
+
   def receive = {
-    case Ping => {
+    case Ping =>
       count += 1
+      log.info(s"Ping received. Count = $count now.")
       count match {
-        case x if x < 1000 => sender ! Stop
+        case x if x > 10 => sender ! Stop
         case _ => sender ! Pong(count)
-    }
+      }
   }
 }
 
-class Client extends Actor {
+class Client extends Actor with ActorLogging {
+  val server = context.actorSelection("akka://test-system/user/server*")
+  server ! Ping
   def receive = {
-    case Pong => sender ! Ping
-    case Stop => println("[Client]: Stopped.")
+    case pong @ Pong(count) =>
+      log.info(s"$pong received")
+      sender ! Ping
+    case Stop => log.info("Stop received")
   }
 }
 
 object Test extends App {
-  val system = ActorSystem("test1")
-  val greeter = system.actorOf(Props[Greeter], "greeter")
-  greeter.tell(WhoToGreet("kot"), ActorRef.noSender)
-  inbox.send(greeter, Greet)
-  val Greeting(message1) = inbox.receive(5 seconds)
-  println(s"Greeting: $message1")
-
-  for (i <- 1 to 1000000) {
-    greeter ! WhoToGreet(i.toString)
-  }
+  //val config = ConfigFactory.parseFile(new File(".\\resources\\application.conf"))
+  val system = ActorSystem("test-system") //, config)
+  system.actorOf(Props[Server], "server1")
+  system.actorOf(Props[Server], "server2")
+  system.actorOf(Props[Client], "client")
+  system.shutdown()
 }
